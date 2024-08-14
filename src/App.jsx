449406a5698e956a4534fe91f9315ed384fe5c87
נתאי קסנר <nitayke@gmail.com>
2024-08-14 10:52:21 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Fireworks from "@fireworks-js/react";
 import "./styles/app.css";
-import Counter from "./Counter";
+import Cake from "./Cake";
+import Card from "./Card";
 
 export default function App() {
   return (
     <div className="app">
-      <h1>יומולדת לנועה נחמה</h1>
-      <Counter />
+      {/* <h1>יומולדת לנועה נחמה</h1> */}
       {/* <Fireworks /> */}
+      {/* <Cake /> */}
+      <Card />
     </div>
   );
 }
