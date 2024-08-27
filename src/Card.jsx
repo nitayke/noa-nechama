4a527287711a87a2e24d9e574a86b365a00c700e
Nitay Kessner <63166525+nitayke@users.noreply.github.com>
2024-08-27 22:40:35 +0300

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,26 +1,28 @@
-import "./styles/card.css";
-import React from "react";
+import './styles/card.css';
+import React from 'react';
 
 export default () => (
-    <div className="birthdayCard">
-        <div className="cardFront">
-            <h3 className="happy">יומולדת שמח לנועה נחמה!</h3>
-            <div className="balloons">
-                <div className="balloonOne" />
-                <div className="balloonTwo" />
-                <div className="balloonThree" />
-                <div className="balloonFour" />
-            </div>
+  <div className='card-body'>
+    <div className='birthdayCard'>
+      <div className='cardFront'>
+        <h3 className='happy'>יומולדת שמח לנועה נחמה!</h3>
+        <div className='balloons'>
+          <div className='balloonOne' />
+          <div className='balloonTwo' />
+          <div className='balloonThree' />
+          <div className='balloonFour' />
         </div>
-        <div className="cardInside">
-            <h3 className="back">מזל טוב נועה נחמה!</h3>
-            <p>לנועה נחמה היקרה,</p>
-            <p>
-                יומולדת שמח!! רציתי לאחל לך את מיטב הברכות ליום המיוחד הזה:
-                אושר, עושר, בריאות איתנה, שפע, ישועות ועוד. מי ייתן ותמשיכי
-                להיות מקסימה כמו שאת, אשת חיל, ומשו.
-            </p>
-            <p className="name">נתאי</p>
-        </div>
+      </div>
+      <div className='cardInside'>
+        <h3 className='back'>מזל טוב נועה נחמה!</h3>
+        <p className='card-p'>לנועה נחמה היקרה,</p>
+        <p className='card-p'>
+          יומולדת שמח!! רציתי לאחל לך את מיטב הברכות ליום המיוחד הזה: אושר,
+          עושר, בריאות איתנה, שפע, ישועות ועוד. מי ייתן ותמשיכי להיות מקסימה כמו
+          שאת, אשת חיל, ומשו.
+        </p>
+        <p className='card-name'>נתאי</p>
+      </div>
     </div>
+  </div>
 );
